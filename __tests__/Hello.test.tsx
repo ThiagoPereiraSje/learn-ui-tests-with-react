25ffd7210ecad3108c62ce895dc6386ca1b01ab7
Thiago Pereira <thiagopereiramail@gmail.com>
2022-02-12 21:00:53 +0000

    create snapshot of component

--- __tests__/Hello.test.tsx
+++ __tests__/Hello.test.tsx
@@ -3,7 +3,13 @@ import { render } from "@testing-library/react";
 import Hello from "../src/packages/components/Hello";
 
 describe("Testing Hello Component", () => {
-  test("your content should be 'Hello world!'", () => {
+  test("Should be render correctly", () => {
+    const { container } = render(<Hello />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("Your content should be 'Hello world!'", () => {
     const { container } = render(<Hello />);
     expect(container.textContent).toBe("Hello world!");
   });
